733df0ff2999c4f5b0731d06a3a06a06f7e2aeaa
adamba-cloud <philipcraig364@gmail.com>
2026-05-31 08:19:38 +0300

    Create metaapi.service.ts

---
+++ src/modules/copyengine/metaapi.service.ts
@@ -0,0 +1,36 @@
+import MetaApi, { MetatraderAccount } from 'metaapi.cloud-sdk';
+import { prisma } from '../../config/database';
+import { encrypt } from '../../config/encryption';
+
+const token = process.env.METAAPI_TOKEN!;
+export const metaApi = new MetaApi(token);
+
+export class MetaApiService {
+  static async provisionCloudTerminal(userId: string, login: string, pass: string, server: string) {
+    const encryptedPassword = encrypt(pass);
+
+    const account = await metaApi.metatraderAccountApi.createAccount({
+      name: `PESAMATRIX_${login}`,
+      type: 'cloud',
+      login: parseInt(login),
+      password: pass,
+      server: server,
+      platform: 'mt5',
+      application: 'MetaApi',
+      magic: 882026
+    });
+
+    await prisma.mt5Account.create({
+      data: {
+        userId,
+        login,
+        password: encryptedPassword,
+        server,
+        metaId: account.id,
+        accountType: 'SLAVE'
+      }
+    });
+
+    return account.id;
+  }
+}
